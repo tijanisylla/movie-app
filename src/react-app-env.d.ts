475a1e84d46d43b7e3d8699b0c099bdc24de73ba
Tijani Sylla <tijani.sylla1@gmail.com>
2023-04-30 20:39:18 -0500

    gh pages3 commit

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="react-scripts" />
-
-// swcalre video mp4
-// https://www.youtube.com/watch?v=6Zn8t5u6h54
-declare module "*.mp4" {
-  const src: string;
-  export default src;
-}
